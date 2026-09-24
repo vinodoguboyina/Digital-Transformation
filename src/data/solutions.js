0a0dc82b02716b9b9e0d5d9bdcd4d4ef5fa2d3af
vinodoguboyina <vinodoguboyina123@gmail.com>
2026-09-24 09:54:27 +0530

    Remove solution bsfb

--- src/data/solutions.js
+++ src/data/solutions.js
@@ -80,13 +80,6 @@ export const solutions = [
     url: "",
     image: img_indiq,
   },
-  {
-    id: "e9f2b780-d71f-48cd-b574-6073fa172495",
-    title: "bsfb",
-    text: "dsbd",
-    url: "",
-    image: "",
-  },
 ]
 
 if (import.meta.hot) {
